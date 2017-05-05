5941d694a30bd72642b295cdbedbb4762c6ba8fe
Wang Weiming <873294167@qq.com>
2017-05-05 15:56:55 +0800

    'api'

---
+++ WebApplication1/Areas/Send/SendAreaRegistration.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApplication1.Areas.Send
+{
+    public class SendAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Send";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Send_default",
+                "Send/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+
+            GlobalConfiguration.Configuration.Routes.MapHttpRoute("sendapi", "api/"+AreaName+"/{controller}/{id}");
+        }
+    }
+}
